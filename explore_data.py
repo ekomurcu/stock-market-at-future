c99a6d09b5dc44e704609ec608785235425d0b67
Egemen Yiğit Kömürcü <ekomurcu@sabanciuniv.edu>
2020-05-14 19:17:26 +0200

    Refactor explore_data.py

--- explore_data.py
+++ explore_data.py
@@ -1,23 +1,26 @@
-
 def explore_data(data):
     # Giving basic information regarding the dataset such as shape, data types and descriptive statistics that summarize columns
-    # print shape
-    print('Data Dimensionality: ', data.shape)
-    print("There are " + str(data.shape[0]) + " rows and " + str(data.shape[1]) + " attributes in historical stock prices data")
+    print("Data Dimensionality: ", data.shape)
+    print("There are " + str(data.shape[0]) + " rows and " + str(
+        data.shape[1]) + " attributes in historical stock prices data")
 
     # print attribute names
-    print('Attribute Names: ', data.columns)
+    print("Attribute Names: ", data.columns)
 
     print("The number of stocks are: " + str((data['ticker'].nunique())))
 
     # print first 5 rows in your dataset
-    print('Head of Data: ')
-    data.head()
+    print("Head of Data: ")
+    print(data.head())
 
     # print data types of the columns
-    print('Data types: ', data.dtypes)
+    print("Data types: ")
+    print(data.dtypes)
 
-    data.isnull().sum()  # NaN counts in each column
+    print("NaN counts in each column: ")
+    print(data.isnull().sum())  # NaN counts in each column
+    print("There is no missing value at any attribute or row so we are fine to proceed without preprocessing.")
 
     # Descriptive statistics of taxitrips data
-    data.describe()
+    print("Descriptive statistics of taxitrips data:")
+    print(data.describe())
